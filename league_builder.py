11c562606416c1003eb6a2e1cba0957f4527f493
BG <bgivens@pm.me>
2018-10-31 19:05:48 -0400

    Increase PEP8 compliance

--- league_builder.py
+++ league_builder.py
@@ -1,4 +1,3 @@
-
 # -*- coding: utf-8 -*-
 """
 Python Web Development Techdegree
@@ -12,61 +11,60 @@ import csv
 teams = {
     "Sharks": [],
     "Dragons": [],
-    "Raptors": []
+    "Raptors": [],
 }
 
-FILEIN = "soccer_players.csv"
-FILEOUT = "teams.txt"
-NUMTEAMS = len(teams)
+FILE_IN = "soccer_players.csv"
+FILE_OUT = "teams.txt"
+NUM_TEAMS = len(teams)
 
 
-def readplayers():
+def read_players():
     "Read in player dictionary from file"
-    with open(FILEIN, 'r', newline='\n') as csvfile:
-        players = list(csv.DictReader(csvfile, delimiter=','))
+    with open(FILE_IN, 'r', newline='\n') as csv_file:
+        players = list(csv.DictReader(csv_file, delimiter=','))
     return players
 
 
-def sortedbyexperience(players):
+def sort_by_experience(players):
     "Sort player records by experience level - yes or no"
     return sorted(players, key=lambda record: record["Soccer Experience"])
 
 
-def teamcategorize(sortedplayers):
+def categorize_by_team(sortedplayers):
     "Categorize sorted players into team lists"
-    teamlist = list(teams.keys())
-    for idx, record in enumerate(sortedplayers):
-        team = teamlist[idx % NUMTEAMS]
+    team_list = list(teams.keys())
+    for idx, record in enumerate(sorted_players):
+        team = team_list[idx % NUM_TEAMS]
         teams[team].append(record)
     return teams
 
 
-def writeleagueroster(teams):
+def write_league_roster(teams):
     "Output team information to a file"
-    with open(FILEOUT, 'w') as textfile:
+    with open(FILE_OUT, 'w') as text_file:
         for team, records in teams.items():
-            print("\n" + team, file=textfile)
-            #print(20*"-", file=textfile)
+            print("\n" + team, file=text_file)
             for record in records:
                 seq = (
                     record["Name"],
                     record["Soccer Experience"],
                     record["Guardian Name(s)"])
-                print(", ".join(seq), file=textfile)
+                print(", ".join(seq), file=text_file)
 
 
-def writeletter(teams):
+def write_letter(teams):
     "Write letter for each player to a file"
     for team, records in teams.items():
         for record in records:
             first_name, last_name = record["Name"].split()
             file_name = "{}_{}.txt".format(first_name, last_name)
-            with open(file_name, 'w') as textfile:
-                str = createletter(team, record)
-                print(str, file=textfile)
+            with open(file_name, 'w') as text_file:
+                str = create_letter(team, record)
+                print(str, file=text_file)
 
 
-def createletter(team, record):
+def create_letter(team, record):
     "Create letter to parents given player record and team info"
     return """Dear {},
 Your child, {}, is on the {} team.
@@ -76,8 +74,8 @@ The Soccer League""".format(record["Guardian Name(s)"], record["Name"], team)
 
 
 if __name__ == "__main__":
-    players = readplayers()
-    sortedplayers = sortedbyexperience(players)
-    teams = teamcategorize(sortedplayers)
-    writeleagueroster(teams)
-    writeletter(teams)
+    players = read_players()
+    sorted_players = sort_by_experience(players)
+    teams = categorize_by_team(sorted_players)
+    write_league_roster(teams)
+    write_letter(teams)
